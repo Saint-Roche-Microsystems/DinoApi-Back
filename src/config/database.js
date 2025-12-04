543d9b450a0239491bbad307712eb18dae69a1c8
Olivier Paspuel <olivier.paspuel@gmail.com>
2025-12-04 18:42:28 -0500

    feat: modify exported api to handle serverless environment (WIP)

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,35 @@
 const mongoose = require('mongoose');
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB conectado exitosamente');
-  } catch (error) {
-    console.error('Error conectando a MongoDB:', error.message);
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => {
+      console.log('MongoDB conectado exitosamente');
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 };
 
 module.exports = connectDB;
